eae2848d4bbdf4f6be01306df7cc22db0fc20919
sai <saichandrikasasi@gmail.com>
2025-02-20 15:40:20 +0530

    variables

--- function.sh
+++ function.sh
@@ -5,6 +5,10 @@ R="\e[31m"
 G="\e[32m"
 N="\e[33m"
 
+VALIDATE(){
+echo "exit status $1"
+}
+
 if [ $Userid -ne 0 ]
 then
  echo  -e "plaes try to access with $R root prevelages "
@@ -12,30 +16,38 @@ then
 fi
 
  dnf list installed git -y
+ VALIDATE $? 
 
  if [ $? -ne 0 ]
  then 
   echo "git is not installed ..going to intall"
   dnf install git -y 
+   if [ $? -ne 0 ]
+   then 
+    echo  -e "GIT is not intalled $R try again"
+    exit 1
+   else
+    echo -e  "$G git installation is  success"
+   fi 
 
  else 
   echo -e "$G git is already installed nothing to do it"
  fi  
 
- dnf list installed mysq -y
+#  dnf list installed mysq -y
 
- if [ $? -ne 0 ]
- then 
-  echo -e "mysql is $R not installed .. $G going to intall"
-   dnf install mysql -y 
-  if [ $? -ne 0 ]
-  then 
-   echo  -e "mysql is not intalled $R try again"
-   exit 1
-  else
-   echo -e  "$G mysql installation is  success"
-  fi 
+#  if [ $? -ne 0 ]
+#  then 
+#   echo -e "mysql is $R not installed .. $G going to intall"
+#    dnf install mysql -y 
+#   if [ $? -ne 0 ]
+#   then 
+#    echo  -e "mysql is not intalled $R try again"
+#    exit 1
+#   else
+#    echo -e  "$G mysql installation is  success"
+#   fi 
 
- else 
-  echo -e "mysql is $G already installed ....$N nothing to do it"
- fi 
+#  else 
+#   echo -e "mysql is $G already installed ....$N nothing to do it"
+#  fi 
