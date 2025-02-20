e5d0df1c7be13ec5be2757cc0e46848efd1f7430
sai <saichandrikasasi@gmail.com>
2025-02-20 12:17:42 +0530

    variables

--- array.sh
+++ array.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-FRUITS=("APLLE" "Grapes" "ORANGE" "KIWI") 
+FRUITS=("APLLE" "Grapes" "ORANGE" "KIWI")
+
+echo " First Fruit is : $FRUITS[0]"
+echo " Second Fruit is :$FRUITS[1]"
+echo " Third Fruit is :$FRUITS[2]"
+echo " Fourth Fruit is :$FRUITS[3]"
