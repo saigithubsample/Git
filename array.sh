97dfdc2e101b117fc6cd5109a5b6f6a0c0c70015
sai <saichandrikasasi@gmail.com>
2025-02-20 12:19:26 +0530

    variables

--- array.sh
+++ array.sh
@@ -2,7 +2,9 @@
 
 FRUITS=("APLLE" "Grapes" "ORANGE" "KIWI")
 
-echo " First Fruit is : $FRUITS[0]"
-echo " Second Fruit is :$FRUITS[1]"
-echo " Third Fruit is :$FRUITS[2]"
-echo " Fourth Fruit is :$FRUITS[3]"
+echo " First Fruit is : ${FRUITS[0]}"
+echo " Second Fruit is :${FRUITS[1]}"
+echo " Third Fruit is :${FRUITS[2]}"
+echo " Fourth Fruit is :${FRUITS[3]}"
+
+echo "" ALL friuts are : ${FRUITS[@]}
