04c08da2979bb5fbc041cbbb0d5110e02583dcd5
sai <saichandrikasasi@gmail.com>
2025-02-20 16:13:55 +0530

    variables

--- loops.sh
+++ loops.sh
@@ -1,7 +1,32 @@
 #!/bin/bash
 
-for i in {1..59}
-do
- echo "$i"
+Userid=$(id -u)
 
-done 
+R="\e[31m"
+G="\e[32m"
+N="\e[33m"
+
+cHECK_ROOT(){
+if [ $Userid -ne 0 ]
+then
+ echo "try to access with root prevelages"
+ exit 1
+fi
+}
+
+VALIDATE(){
+ if [ $? -ne 0 ]
+ then 
+  echo "$2 is failed"
+  exit 1
+ else 
+  echo "$2 is success"
+}
+
+CHECK_ROOT
+
+for package in $@
+do
+
+echo $package
+done
