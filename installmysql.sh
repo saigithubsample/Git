f00d20e2b76dcf78339c2819fae53d1c0ee47390
sai <saichandrikasasi@gmail.com>
2025-02-20 13:38:34 +0530

    variables

--- installmysql.sh
+++ installmysql.sh
@@ -1,17 +1,27 @@
 #!/bin/bash
 
-# Userid=$(id -u)
+Userid=$(id -u)
 
-# if [ $Userid -ne 0 ] 
-# then
-# echo "run the script with root user access"
+if [ $Userid -ne 0 ] 
+then
+echo "run the script with root user access"
 
-# fi
+exit 1
+
+fi
 
 dnf list intalled mysql
 if [ $? -ne 0 ]
   then
    echo "mysql is not installed..... going to install"
+   dnf install mysql -y
+   if [ $? -ne 0 ]
+     then 
+      echo "installation of my sql not success"
+      exit 1
+     else
+      echo "installation of my sql is success"  
+    fi 
 
   else
     echo "mysql is already installed ..nothing to do"
