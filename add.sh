0447f5bc70f38be6684cecd9e478137bcb6be801
sai <saichandrikasasi@gmail.com>
2025-02-20 13:03:18 +0530

    variables

--- add.sh
+++ add.sh
@@ -9,10 +9,11 @@ sum=$(($Number1+$Number2))
 if [ $sum -gt 100 ]
 
    then
+   echo "the sum is $sum"
    echo " sum is greather than 100"
 
    else
-
+    echo "the sum is $sum"
    echo "sum is lessthen 100"
 
 fi
