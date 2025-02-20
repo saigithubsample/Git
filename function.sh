82b191f015af07ae839d72846f6e55a3c47b83f8
sai <saichandrikasasi@gmail.com>
2025-02-20 15:48:57 +0530

    variables

--- function.sh
+++ function.sh
@@ -21,38 +21,26 @@ then
 fi
 
  dnf list installed git -y
- VALIDATE $? "listing git" 
+  
 
-#  if [ $? -ne 0 ]
-#  then 
-#   echo "git is not installed ..going to intall"
-#   dnf install git -y 
-#    if [ $? -ne 0 ]
-#    then 
-#     echo  -e "GIT is not intalled $R try again"
-#     exit 1
-#    else
-#     echo -e  "$G git installation is  success"
-#    fi 
+ if [ $? -ne 0 ]
+ then 
+  echo "git is not installed ..going to intall"
+  dnf install git -y 
+   VALIDATE $? "installing git"
 
-#  else 
-#   echo -e "$G git is already installed nothing to do it"
-#  fi  
+ else 
+  echo -e "$G git is already installed nothing to do it"
+ fi  
 
-#  dnf list installed mysq -y
+ dnf list installed mysq -y
 
-#  if [ $? -ne 0 ]
-#  then 
-#   echo -e "mysql is $R not installed .. $G going to intall"
-#    dnf install mysql -y 
-#   if [ $? -ne 0 ]
-#   then 
-#    echo  -e "mysql is not intalled $R try again"
-#    exit 1
-#   else
-#    echo -e  "$G mysql installation is  success"
-#   fi 
+ if [ $? -ne 0 ]
+ then 
+   echo -e "mysql is $R not installed .. $G going to intall"
+   dnf install mysql -y 
+   VALIDATE $? "installing mysql"
 
-#  else 
-#   echo -e "mysql is $G already installed ....$N nothing to do it"
-#  fi 
+ else 
+  echo -e "mysql is $G already installed ....$N nothing to do it"
+ fi 
