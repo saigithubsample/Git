79c04ce5599acf26939330bf6105ac966189af8b
sai <saichandrikasasi@gmail.com>
2025-02-20 12:10:14 +0530

    variables

---
+++ read.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "enter the username"
+
+read USERNAME
+
+echo "user name u entered is: $USERNAME"
+
+echo "enter the password "
+
+read PASSWORD
+
+echo " password u entered is : $PASSWORD"
